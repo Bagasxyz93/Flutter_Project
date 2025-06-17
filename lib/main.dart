d5ac281e3c1680cbc81e06746592e5f6b4ff5a71
Bagas <bagas.fathurrahman@gmail.com>
2025-06-17 16:06:49 +0700

    tes merge

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PDAM Baubau',
+      title: 'PDAM ',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         scaffoldBackgroundColor: Colors.white,
